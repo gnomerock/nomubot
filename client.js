9d6813fb8bfa81a63e021f62b552cf29c94e03f5
Nuttakorn Buajarern <nuttakorn.buajarern@gmail.com>
2022-09-11 20:08:13 +0700

    refractor client to seperate file and prepare for run express in main

---
+++ client.js
@@ -0,0 +1,45 @@
+const fs = require('node:fs');
+const path = require('node:path');
+
+// Require the necessary discord.js classes
+const { Client, Collection, GatewayIntentBits } = require('discord.js');
+const { token } = require('./config.json');
+
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+client.commands = new Collection();
+
+// ******* auto read commands
+
+const commandsPath = path.join(__dirname, 'commands');
+const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+
+//loop read files into commands
+for (const file of commandFiles) {
+  const filePath = path.join(commandsPath, file);
+  const command = require(filePath);
+  // Set a new item in the Collection
+  // With the key as the command name and the value as the exported module
+  client.commands.set(command.data.name, command);
+}
+
+// **** auto read events
+const eventsPath = path.join(__dirname, 'events');
+const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js'));
+
+for (const file of eventFiles) {
+  const filePath = path.join(eventsPath, file);
+  const event = require(filePath);
+  if (event.once) {
+    client.once(event.name, (...args) => event.execute(...args));
+  } else {
+    client.on(event.name, (...args) => event.execute(...args));
+  }
+}
+
+// Login to Discord with your client's token
+client.login(token);
+
+module.exports = {
+  client
+}
