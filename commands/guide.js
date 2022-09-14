eeac57ac7bb9548ab3c97af3506a0bba656c602b
Nuttakorn Buajarern <nuttakorn.buajarern@gmail.com>
2022-09-15 04:15:53 +0700

    added sample guide slash command

--- commands/guide.js
+++ commands/guide.js
@@ -1,15 +1,60 @@
 const { SlashCommandBuilder } = require('discord.js');
 
+const raids = [
+  { name: 'p5s', fullname: 'Pandaemonium Abyssos: The Fifth Circle'},
+  { name: 'p6s', fullname: 'Pandaemonium Abyssos: The Sixth Circle'},
+  { name: 'p7s', fullname: 'Pandaemonium Abyssos: The Seventh Circle'},
+  { name: 'p8s', fullname: 'Pandaemonium Abyssos: The Eighth Circle'},
+]
+
 module.exports = {
   data: new SlashCommandBuilder()
     .setName('guide')
+    .setNameLocalizations({
+      th: 'ไกด์'
+    })
     .setDescription('search ffxiv raiding guide')
+    .setDescriptionLocalizations({
+      th: 'ค้นหาไกด์วิธีเล่นเหรด ffxiv',
+    })
     .addStringOption(option =>
-      option.setName('raid')
+      option
+        .setName('raid')
+        .setNameLocalizations({
+          th: 'ตัวย่อชื่อเหรด'
+        })
         .setDescription('input raid abbreviation name.')
-        .setAutocomplete(true));,
-  async execute(interaction) {
-    console.log(`[+] user ${interaction.user.tag}:${interaction.user.id} looking for ${}`)
-    await interaction.reply(`Server name: ${interaction.guild.name}\nTotal members: ${interaction.guild.memberCount}`);
-  },
+        .setDescriptionLocalizations({
+          th: 'ใส่ตัวย่อชื่อเหรด เช่น p8s, uwu, tea',
+        })
+        .setAutocomplete(true)
+    ),
+    async execute(interaction) {
+      
+      if (interaction.isAutocomplete()) {
+
+        const focusedValue = interaction.options.getFocused(true);
+
+        let choices = []
+        if (focusedValue.name === 'raid') {
+          choices = raids
+        }
+
+        const filtered = choices.filter(choice => choice.name.startsWith(focusedValue.value))
+
+        await interaction.respond(
+          filtered.map(choice => ({ name: choice.name, value: choice.name, description: 'test' })),
+        )
+
+      }else{
+
+        console.log(`[+] ${interaction.user.tag} is getting ${interaction.options.get('raid')} guide.`)
+
+        await interaction.reply({
+          content: `[+] ${interaction.user.tag} is getting ${interaction.options.get('raid').value} guide.`,
+          ephemeral: true
+        });
+
+      }
+    },
 };
--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -4,7 +4,7 @@ module.exports = {
 
     console.log(`[+] ${interaction.user.tag} in #${interaction.channel.name} triggered an interaction.`);
 
-    if (!interaction.isChatInputCommand()) return;
+    if (!interaction.isChatInputCommand() && !interaction.isAutocomplete()) return;
 
     const command = interaction.client.commands.get(interaction.commandName);
 
