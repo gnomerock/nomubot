c5494349ffe277bc800994323caec67eab6dbd45
Nuttakorn Buajarern <nuttakorn.buajarern@gmail.com>
2023-08-02 04:00:11 +0700

    testembed

--- commands/guide.js
+++ commands/guide.js
@@ -1,10 +1,21 @@
-const { SlashCommandBuilder } = require('discord.js');
+const { 
+  SlashCommandBuilder,
+  EmbedBuilder
+} = require('discord.js');
 
 const raids = [
+  { name: 'p1s', fullname: 'Pandaemonium Abyssos: The First Circle'},
+  { name: 'p2s', fullname: 'Pandaemonium Abyssos: The Second Circle'},
+  { name: 'p3s', fullname: 'Pandaemonium Abyssos: The Third Circle'},
+  { name: 'p4s', fullname: 'Pandaemonium Abyssos: The Forth Circle'},
   { name: 'p5s', fullname: 'Pandaemonium Abyssos: The Fifth Circle'},
   { name: 'p6s', fullname: 'Pandaemonium Abyssos: The Sixth Circle'},
   { name: 'p7s', fullname: 'Pandaemonium Abyssos: The Seventh Circle'},
   { name: 'p8s', fullname: 'Pandaemonium Abyssos: The Eighth Circle'},
+  { name: 'p9s', fullname: 'Pandaemonium Abyssos: The Ninth Circle'},
+  { name: 'p10s', fullname: 'Pandaemonium Abyssos: The Tenth Circle'},
+  { name: 'p11s', fullname: 'Pandaemonium Abyssos: The Eleventh Circle'},
+  { name: 'p12s', fullname: 'Pandaemonium Abyssos: The Twelfth Circle'},
 ]
 
 module.exports = {
@@ -47,9 +58,29 @@ module.exports = {
         )
 
       }else{
+        let selectedRaid = interaction.options.get('raid').value
+        console.log(`[+] ${interaction.user.tag} is getting ${selectedRaid} guide.`)
+        const embed = new EmbedBuilder()
+          .setColor(0x0099FF)
+          .setTitle('Some title')
+          .setURL('https://discord.js.org/')
+          .setAuthor({ name: 'Some name', iconURL: 'https://i.imgur.com/AfFp7pu.png', url: 'https://discord.js.org' })
+          .setDescription('Some description here')
+          .setThumbnail('https://i.imgur.com/AfFp7pu.png')
+          .addFields(
+            { name: 'Regular field title', value: 'Some value here' },
+            { name: '\u200B', value: '\u200B' },
+            { name: 'Inline field title', value: 'Some value here', inline: true },
+            { name: 'Inline field title', value: 'Some value here', inline: true },
+          )
+          .addFields({ name: 'Inline field title', value: 'Some value here', inline: true })
+          .setImage('https://i.imgur.com/AfFp7pu.png')
+          .setTimestamp()
+          .setFooter({ text: 'Some footer text here', iconURL: 'https://i.imgur.com/AfFp7pu.png' });
 
         await interaction.reply({
-          content: `[+] ${interaction.user.tag} is getting ${interaction.options.get('raid').value} guide.`,
+          // content: `[+] ${interaction.user.tag} is getting ${interaction.options.get('raid').value} guide.`,
+          embeds: [embed],
           ephemeral: true
         });
 
--- commands/item.js
+++ commands/item.js
@@ -5,7 +5,7 @@ module.exports = {
     .setName('item')
     .setDescription('ffxiv item information'),
   async execute(interaction) {
-    let response = await fetch(`https://xivapi.com/item/1675?private_key=56399de741e7447b85de39f3b81e09f65ac30b0a370a485b81f3fc84a14727b8`)
+    let response = await fetch(`https://xivapi.com/item/1675?private_key=56399de741e7447b85de39f3b81e09f65ac30b0a370a485b81f3fc84a14727b8&pretty=1`)
     
     let data = await response.json().data
     console.log({data})
